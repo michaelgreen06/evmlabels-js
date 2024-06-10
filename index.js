77f7a327a168e8564f10a41acfd6b6744321e098
Michael Green <michaelgreen06@gmail.com>
2024-06-10 11:34:41 -0600

    decided to work on elysia ts version instead

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = 3000;
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log(req, res);
+//req.body.address will have the address that needs to be searched for
+app.post("/", (req, res) => {
+  //todo validation - if the address isn't a certain format 0x... return a 400 w/ 'incorrect format'
+  const searchAddress = req.body.address;
+  if (!searchAddress) {
+    return res.status(400).json({ error: "address is required" });
+  }
+  const jsonFiles = ["1inch.json", "aave.json"];
+  const results = [];
+
+  jsonFiles.forEach((file) => {
+    const filePath = path.join(__dirname, file);
+    const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+    if (JSON.stringify(data).includes(searchString)) {
+      results.push(file);
+    }
+  });
+
+  res.json({ filesContainingString: results });
 });
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
 
-[
-  {
-    parent: coinbase,
-    nametags: {
-      nametag1: "aave_hot_1",
-      nametag2: "prime funder",
-    },
-  },
-  {
-    parent: binance,
-    nametags: {
-      nametag1: "cex account 1",
-      nametag2: "prime dunder",
-    },
-  },
-];
+//Thinking about the JSON structure of the  response
+// [
+//   {
+//     parent: coinbase,
+//     nametags: {
+//       nametag1: "aave_hot_1",
+//       nametag2: "prime funder",
+//     },
+//   },
+//   {
+//     parent: binance,
+//     nametags: {
+//       nametag1: "cex account 1",
+//       nametag2: "prime dunder",
+//     },
+//   },
+// ];
